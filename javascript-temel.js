1b8af29c4a64ef52e2d369d5371a1a5ce8de70e2
orkan6 <158033955+orkan6@users.noreply.github.com>
2024-11-22 13:30:56 +0300

    Create javascript-temel.js

---
+++ javascript-temel.js
@@ -0,0 +1,28 @@
+const = sabit değişken
+let ve var = düzenlenebilen değişken
+; koymayı unutma
+
+// window prompt ile input alma
+let isim;
+isim = window.prompt("Adını gir:);
+
+// yazdırma
+console.log(); // ile yapılır
+
+// veri tipi olarak almak 
+
+let Number(yas); // yaş değişkeni sayı olarak alınır
+// veritipleri olarak ; Number,String,Boolean var
+console.log(yas , typeof yas); // typeof fonksiyonu değişkenin veritipini gösterecek
+
+// if statements
+let yas = 30;
+
+if(yas > 20){
+  console.log("Yaşınız 20 den büyük);
+}
+ //aynı şekilde else if (pythonda elif) , ve else koşulları da aynı mantıkta çalışıyor
+
+//mantıksal operatörler
+
+
